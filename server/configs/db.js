fec8c39493aea6b145a2db1771abe1a3f014c4ff
Hoàng Tùng <tung062005@gmail.com>
2026-03-31 00:30:00 +0700

    fix db

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    mongoose.connection.on("connected", () => {
-      console.log("MongoDB connected successfully");
-    });
     await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+    console.error("MongoDB connection error:", error);
   }
 };
 
